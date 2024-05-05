28d1968be4c1f12dd2e4fe3dd2447d7f6417dc68
sayed mudassir <sayedmuddassir68@gmail.com>
2024-05-05 16:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    let firstname = form.firstName.value;
+    let lastname = form.lastName.value;
+    let phoneNum = form.PhoneNumber.value;
+    let email = form.emailID.value;
+
+    let alertString = "First Name: "+firstname + " Last Name: " +lastname+ " Phone Number: "+ phoneNum +" Email ID: "+ email;
+    alert(alertString);
+})
